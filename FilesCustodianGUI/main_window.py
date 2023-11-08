256c9b49044ab637a56edb000fbbe60b3a695353
Mansur Azatbek <mansur62624@gmail.com>
2023-11-08 15:27:29 +0600

    feat: settings 2 panel

--- FilesCustodianGUI/main_window.py
+++ FilesCustodianGUI/main_window.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
-from settings_window import SettingsWindow
+from settings_window import SettingsWindow,CopyPanel
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
@@ -11,19 +11,28 @@ class MyMainWindow(QMainWindow):
 
         menubar = self.menuBar()
 
-        exit_action = QAction("Выход", self)
+        exit_action = QAction(self.tr("Выход"), self)
         exit_action.triggered.connect(self.close)
 
         settings_menu = menubar.addMenu(self.tr("Настройки"))
 
-        open_settings_action = QAction(self.tr("Открыть настройки"), self)
+        open_settings_action = QAction(self.tr("Язык и тема"), self)
         open_settings_action.triggered.connect(self.open_settings)
 
+        open_copy_panel_action = QAction(self.tr("Копирование файлов"), self)
+        open_copy_panel_action.triggered.connect(self.open_copy_panel)
+
+
         settings_menu.addAction(open_settings_action)
+        settings_menu.addAction(open_copy_panel_action)
 
     def open_settings(self):
         settings_window = SettingsWindow()
         settings_window.exec_()
+
+    def open_copy_panel(self):
+        copy_panel = CopyPanel()
+        copy_panel.exec_()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- FilesCustodianGUI/settings_window.py
+++ FilesCustodianGUI/settings_window.py
@@ -1,23 +1,23 @@
-from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, QPushButton, QVBoxLayout,QMainWindow, 
 
 class SettingsWindow(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Настройки")
+        self.setWindowTitle(self.tr("Настройки"))
         self.setGeometry(200, 200, 300, 200)
 
-        theme_label = QLabel("Тема:")
+        theme_label = QLabel(self.tr("Тема:"))
         self.theme_combo = QComboBox()
-        self.theme_combo.addItem("Светлая")
-        self.theme_combo.addItem("Темная")
+        self.theme_combo.addItem(self.tr("Светлая"))
+        self.theme_combo.addItem(self.tr("Темная"))
 
-        language_label = QLabel("Язык:")
+        language_label = QLabel(self.tr("Язык:"))
         self.language_combo = QComboBox()
-        self.language_combo.addItem("Русский")
-        self.language_combo.addItem("Английский")
+        self.language_combo.addItem(self.tr("Русский"))
+        self.language_combo.addItem(self.tr("Английский"))
 
-        save_button = QPushButton("Сохранить")
+        save_button = QPushButton(self.tr("Сохранить"))
         save_button.clicked.connect(self.save_settings)
 
         layout = QVBoxLayout()
@@ -36,3 +36,24 @@ class SettingsWindow(QDialog):
 if __name__ == "__main__":
     settings_window = SettingsWindow()
     settings_window.exec_()
+
+
+class CopyPanel(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle(self.tr("Панель копирования файлов"))
+        self.setGeometry(100, 100, 400, 300)
+
+        menubar = self.menuBar()
+
+        # Создаем меню "Файл"
+        file_menu = menubar.addMenu(self.tr("Файл"))
+
+        # Создаем действие "Скопировать"
+        copy_action = QAction(self.tr("Скопировать"), self)
+        file_menu.addAction(copy_action)
+
+        # Создаем действие "Вставить"
+        paste_action = QAction(self.tr("Вставить"), self)
+        file_menu.addAction(paste_action)
